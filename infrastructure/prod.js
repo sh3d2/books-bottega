b415b4c397e4bae4deabddd016b5d63b0fadda67
Karol Kaczmarek <karol.kaczmarek@rst.com.pl>
2016-11-23 08:45:03 +0100

    log drains

--- infrastructure/prod.js
+++ infrastructure/prod.js
@@ -1,13 +1,14 @@
 var heroin = require('heroin-js');
 
-var test = {
-    name: 'test-book-inventory-test',
+var prod= {
+    name: 'test-book-inventory',
     organization: undefined,
     region: 'eu',
     maintenance: false,
     stack: 'cedar-14',
     config_vars: {
-        MONGODB_URI: process.env.MONGODB_URI
+        MONGODB_URI: 'mongodb://heroku_jbj2b46s:voikrvavqqpmo7sj86199mbull@ds159387.mlab.com:59387/heroku_jbj2b46s',
+        DUPA: 'dupa'
     },
     addons: {},
     collaborators: ['sh3d2.pl@gmail.com', 'szymon.skirgajllo@gmail.com'],
@@ -24,9 +25,9 @@ var test = {
     },
     formation: [{process: 'web', quantity: 1, size: 'Free'}],
     log_drains: ['syslog://data.logentries.com:10727'],
-    domains: ['test-book-inventory-test.herokuapp.com']
+    domains: ['test-book-inventory.herokuapp.com']
 };
 
 var configurator = heroin(process.env.HEROKU_API_TOKEN);
 
-configurator(test);
+configurator(prod);
